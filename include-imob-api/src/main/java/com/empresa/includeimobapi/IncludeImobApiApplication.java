cff9c1c4c46a85b5989480e76b56ccf51ad3d1fe
Baldez Jr <abaldezjr@gmail.com>
2021-11-30 15:59:20 -0300

    Primeiro commit

---
+++ include-imob-api/src/main/java/com/empresa/includeimobapi/IncludeImobApiApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.includeimobapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncludeImobApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncludeImobApiApplication.class, args);
+	}
+
+}
---
+++ include-imob-api/src/test/java/com/empresa/includeimobapi/IncludeImobApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.includeimobapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncludeImobApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

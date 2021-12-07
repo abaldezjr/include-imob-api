d6e0488dbbf8728082cc0680c3814b1a5e7f5a4b
Baldez Jr <abaldezjr@gmail.com>
2021-12-07 15:55:57 -0300

    Adicionando sql e segurança de dados das tabelas

---
+++ include-imob-api/src/main/resources/db/migration/V202111301757__reseta-banco-de-dados.sql
@@ -0,0 +1,2 @@
+-- Para atualizar dados default do banco de dados, descomentar linha abaixo.
+-- DELETE FROM flyway_schema_history WHERE 1=0 and description in ('deletando-dados-tabelas', 'inserindo-dados');
--- include-imob-api/src/main/resources/db/migration/V202112062208__inserindo-dados.sql
+++ include-imob-api/src/main/resources/db/migration/V202112062208__inserindo-dados.sql
@@ -1,2 +1,5 @@
 insert INTO tb_pais (NOME, ABREVIACAO) VALUES ('Brasil', 'BR');
 INSERT INTO TB_ESTADO (NOME, ABREVIACAO, ID_PAIS) VALUES ('Rio Grande do Sul', 'RS', (select p.Id_PAIS from TB_PAIS p where Nome = 'Brasil'));
+insert into TB_CIDADE (nome, abreviacao, id_estado) values ('Rio Grande', 'RG', (select es.id_estado from TB_ESTADO es where es.nome = 'Rio Grande do Sul'));
+insert into TB_BAIRRO (nome, id_cidade) values ('Centro', (select ci.id_cidade from TB_CIDADE ci where ci.nome = 'Rio Grande'));
+insert into TB_BAIRRO (nome, id_cidade) values ('Cassino', (select ci.id_cidade from TB_CIDADE ci where ci.nome = 'Rio Grande'));
